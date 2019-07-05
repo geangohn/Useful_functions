47868024ad3a10427435de50339b3f5e357628e2
Ivan Maksimov <geangohn1996@gmail.com>
2019-07-05 17:40:35 +0300

    Create feature_engineering.py

---
+++ feature_engineering.py
@@ -0,0 +1,70 @@
+def add_agg_features(df, gpby_cols, target_col, agg_funcs):
+    '''
+    Creates various target column agg features with given agg functions  
+    '''
+    gpby = df.groupby(gpby_cols)
+    newdf = df[gpby_cols].drop_duplicates().reset_index(drop=True)
+    for agg_name, agg_func in agg_funcs.items():
+        aggdf = gpby[target_col].agg(agg_func).reset_index()
+        aggdf.rename(columns={target_col:target_col+'_'+agg_name}, inplace=True)
+        newdf = newdf.merge(aggdf, on=gpby_cols, how='left')
+    return newdf
+    
+ 
+def create_sales_lag_feats(df, gpby_cols, target_col, lags):
+
+    '''
+    Creates various target column lag features with lags  
+    '''
+    
+    gpby = df.groupby(gpby_cols)
+    for i in lags:
+        df['_'.join([target_col, 'lag', str(i)])] = \
+                gpby[target_col].shift(i).values + np.random.normal(scale=1.6, size=(len(df),))
+    return df
+
+
+def create_sales_rmean_feats(df, gpby_cols, target_col, windows, min_periods=2, 
+                             shift=1, win_type=None):
+                             
+    '''
+    Creates  target column rolling mean features 
+    '''                         
+    
+    gpby = df.groupby(gpby_cols)
+    for w in windows:
+        df['_'.join([target_col, 'rmean', str(w)])] = \
+            gpby[target_col].shift(shift).rolling(window=w, 
+                                                  min_periods=min_periods,
+                                                  win_type=win_type).mean().values)
+    return df
+
+
+def create_sales_rmed_feats(df, gpby_cols, target_col, windows, min_periods=2, 
+                            shift=1, win_type=None):
+                            
+    '''
+    Creates  target column rolling median features 
+    '''  
+    
+    gpby = df.groupby(gpby_cols)
+    for w in windows:
+        df['_'.join([target_col, 'rmed', str(w)])] = \
+            gpby[target_col].shift(shift).rolling(window=w, 
+                                                  min_periods=min_periods,
+                                                  win_type=win_type).median().values)
+    return df
+
+
+def create_sales_ewm_feats(df, gpby_cols, target_col, alpha=[0.9], shift=[1]):
+
+    '''
+    Creates  target column exponentially weighted mean features 
+    '''  
+
+    gpby = df.groupby(gpby_cols)
+    for a in alpha:
+        for s in shift:
+            df['_'.join([target_col, 'lag', str(s), 'ewm', str(a)])] = \
+                gpby[target_col].shift(s).ewm(alpha=a).mean().values
+    return df
