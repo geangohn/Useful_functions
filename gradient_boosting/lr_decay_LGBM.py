44eec613f8037f426d08c0bbb01f488622a46884
Ivan Maksimov <geangohn1996@gmail.com>
2019-08-27 18:44:30 +0300

    Update lr_decay_LGBM.py

--- gradient_boosting/lr_decay_LGBM.py
+++ gradient_boosting/lr_decay_LGBM.py
@@ -1,4 +1,5 @@
 
+## Способ 1
 alpha = 10
 k = 1
 
@@ -11,5 +12,23 @@ gbm = lgb.train(
     
     ###
     callbacks=[lgb.reset_parameter(learning_rate = lambda current_round: alpha * e**(-k*current_round))]
+    ###
+)
+
+
+## Способ 2
+lambda_0 = 0.2
+delta = 0.01
+
+gbm = lgb.train(
+    params,
+    lgb_train,
+    num_boost_round=10,
+    init_model=gbm,
+    valid_sets=lgb_eval,
+    
+    ###
+    callbacks=[lgb.reset_parameter(learning_rate = lambda current_round: 
+                                   lambda_0 - delta if current_round % 100 == 0 else lambda_0)]
     ###
 )
