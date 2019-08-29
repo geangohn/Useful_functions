22bbcb829f4f8f3e5afccd1e106bb7ccde811db2
Ivan Maksimov <geangohn1996@gmail.com>
2019-08-29 18:24:29 +0300

    Create yandex_geocoder.py

---
+++ geocoder/yandex_geocoder.py
@@ -0,0 +1,12 @@
+#!pip install requests
+
+import requests
+
+api_key = '805edf7f-f2dd-430b-bb89-7ae5a7787f8b'   # взять из личного кабинета разработчика на сайте Яндекса
+def get_lat_lon(location_name):
+    response = requests.get('https://geocode-maps.yandex.ru/1.x/?format=json&apikey=' + api_key + '&geocode=' + location_name)
+    response = response.json()
+    coord = response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split(' ')
+    lat, lon = float(coord[1]), float(coord[0])
+    
+    return lat, lon
