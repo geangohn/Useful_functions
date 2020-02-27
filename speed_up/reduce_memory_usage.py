5f7c6a0d7b8d64c482d6b6ac20368b689b621a25
Ivan Maksimov <geangohn1996@gmail.com>
2020-02-27 18:06:58 +0300

    Dropped float16 type

--- speed_up/reduce_memory_usage.py
+++ speed_up/reduce_memory_usage.py
@@ -4,14 +4,15 @@ import numpy as np
 
 def reduce_mem_usage(df):
     """ iterate through all the columns of a dataframe and modify the data type
-        to reduce memory usage.        
+        to reduce memory usage.
     """
-    start_mem = df.memory_usage().sum() / 1024**2
+    start_mem = df.memory_usage().sum() / 1024 ** 2
     print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
-    
+
     for col in df.columns:
-        col_type = df[col].dtype
         
+        col_type = df[col].dtype
+
         if col_type != object:
             c_min = df[col].min()
             c_max = df[col].max()
@@ -23,19 +24,15 @@ def reduce_mem_usage(df):
                 elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
                     df[col] = df[col].astype(np.int32)
                 elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)  
-            else:
-                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
-                    df[col] = df[col].astype(np.float16)
-                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.int64)
+            elif str(col_type)[:5] == 'float':
+                if c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
                     df[col] = df[col].astype(np.float32)
                 else:
                     df[col] = df[col].astype(np.float64)
-        else:
-            df[col] = df[col].astype('category')
 
-    end_mem = df.memory_usage().sum() / 1024**2
+    end_mem = df.memory_usage().sum() / 1024 ** 2
     print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
     print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
-    
+
     return df
